3ecdfa8c9f6a3a3686f668482b77c6d3ecf88854
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-06-01 09:05:29 +0900

    feat: 入力された内容をそのまま出力する
    (cherry picked from commit 93dd417b44de0f0a28d864eb012cb8f59b676463)

--- sll/src/main/scala/step0_repl.scala
+++ sll/src/main/scala/step0_repl.scala
@@ -1,3 +1,11 @@
 object step0_repl extends App {
-  Iterator.continually(scala.io.StdIn.readLine()).takeWhile(_ != null).foreach(println)
+  Iterator.continually(scala.io.StdIn.readLine()).takeWhile(_ != null).foreach(str => println(rep(str)))
+
+  def rep(str: String): String = PRINT(EVAL(READ(str), env = ""))
+
+  def READ(str: String): String = str
+
+  def EVAL(ast: String, env: String): String = ast
+
+  def PRINT(exp: String): String = exp
 }
