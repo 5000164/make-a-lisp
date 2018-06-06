6a7399cb15460f4e6d6b5c4022e3f9dc3e5a3dbf
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-06-06 22:01:33 +0900

    refactor: String 型に read メソッドがあるよりは、 read メソッドが String 型を渡された方が自然なので修正
    (cherry picked from commit c52ba44fa91cd1fbc4de6082878499b62d7c7be9)

--- sll/src/main/scala/step0_repl.scala
+++ sll/src/main/scala/step0_repl.scala
@@ -1,11 +1,9 @@
 object step0_repl extends App {
   Iterator.continually(scala.io.StdIn.readLine("user> ")).takeWhile(_ != null).foreach(str => println(rep(str)))
 
-  def rep(str: String): String = str.read.eval(env = "").print
+  def rep(str: String): String = read(str).eval(env = "").print
 
-  implicit class ReadWrapper(val str: String) {
-    def read: String = str
-  }
+  def read(str: String): String = str
 
   implicit class EvalWrapper(val ast: String) {
     def eval(env: String): String = ast
