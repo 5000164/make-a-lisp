d19a7706872cfeef6d7617d89a170ca3555f5a26
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-06-09 00:06:02 +0900

    feat: フォーマット用の特殊な二文字 ~@ をトークン化できていることを確認する
    (cherry picked from commit f4ff785d1fe9a3b6804448198f4539cca68d92a9)

--- sll/src/test/scala/library/PrinterSpec.scala
+++ sll/src/test/scala/library/PrinterSpec.scala
@@ -7,5 +7,9 @@ class PrinterSpec extends FeatureSpec {
     scenario("シンプルな文字列") {
       assert(Reader.tokenizer("123") === Seq("123"))
     }
+
+    scenario("フォーマットに使用する特殊な 2 文字") {
+      assert(Reader.tokenizer( """(format t "~@d" 10000)""") === Seq("(", "format", "t", """"~@d"""", "10000", ")"))
+    }
   }
 }
