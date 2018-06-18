088d391f0ffc2c2a9c058837271e8de656e85c98
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-06-18 23:20:31 +0900

    fix: クラス名を間違っていたので修正
    (cherry picked from commit 6ac4a61ff8298ebdbff4a66ff6a40af68348fa73)

--- sll/src/test/scala/library/ReaderSpec.scala
+++ sll/src/test/scala/library/ReaderSpec.scala
@@ -2,7 +2,7 @@ package library
 
 import org.scalatest.FeatureSpec
 
-class PrinterSpec extends FeatureSpec {
+class ReaderSpec extends FeatureSpec {
   feature("トークンを取得する") {
     scenario("シンプルな文字列") {
       assert(Reader.tokenizer("123") === Seq("123"))
